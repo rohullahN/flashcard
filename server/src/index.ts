46e3f1c8822b3fbafd52cc09e1d26d01290ad004
rohullahN <41601768+rohullahN@users.noreply.github.com>
2024-05-08 23:45:04 -0400

    implement first endpoint and create an env

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import DeckModel from "./models/Deck";
+import { config } from "dotenv";
+
+config();
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+
+app.post("/decks", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const newDeck = new DeckModel({ title: req.body.title });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
+});
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`listening on port ${PORT}`);
+  app.listen(PORT);
+});
